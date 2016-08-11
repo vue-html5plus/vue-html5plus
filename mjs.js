d88f853dd508b728d76d83302d7f14265be0be60
xiaoqingnian <1028317108@qq.com>
2016-08-11 15:06:04 +0800

    .

--- mjs.js
+++ mjs.js
@@ -3,11 +3,12 @@
  * MJS v0.0.3 (https://github.com/zhaomenghuan/mjs)
  * =====================================================
  */
-(function(w,undefined) {
+(function (w, undefined) {
 	// 构造函数mjs
-	var mjs = function(selector, context) {
-		return new mjs.fn.init(selector, context);
-	}	
+	var mjs = function (selector, context) {
+		return new mjs.fn.init (selector, context);
+	};
+
 	// 构造函数mjs的原型对象
 	mjs.fn = mjs.prototype = {
 		constructor: mjs,
@@ -15,28 +16,25 @@
 			return object instanceof Array;
 		},
 		init: function (selector, context) {
-			if(!selector) { 
-				return this; 
-			}
-			else if(typeof selector === 'object'){
+			if(!selector) {
+				return this;
+			}else if(typeof selector === 'object'){
 				var selector = [selector];
         for (var i = 0; i < selector.length; i++) {
-          this[i] = selector[i];
+            this[i] = selector[i];
         }
         this.length = selector.length;
         return this;
 			}else if(typeof selector === 'function'){
 				return mjs.ready(selector);
-			}
-			else if(typeof selector === 'string'){
+			}else if(typeof selector === 'string'){
 				var re = /<(.+?)>/g;
 				var selector = selector.trim();
 				var context = context || document;
 				var dom = null;
-				
-				if((match = re.exec(selector))!==null){
+				if((match = re.exec(selector)) !== null){
 					return document.createElement(match[1]);
-				}else{
+				} else {
 					var el = context.querySelectorAll(selector);
 					var dom = Array.prototype.slice.call(el);
 					var length = dom.length;
@@ -47,18 +45,18 @@
 					this.selector = selector;
 					this.length = length;
 					return this;
-				}	
-			}	
+				}
+			}
 		},
 		html: function (content) {
 			if(content === undefined && this[0].nodeType === 1){
-        return this[0].innerHTML.trim();
-      }else{
+				return this[0].innerHTML.trim();
+      } else {
         var len = this.length;
 				for (var i = 0; i < len; i++) {
-					this[i].innerHTML = content;
-				}
-				return this;
+    			this[i].innerHTML = content;
+    		}
+    		return this;
       }
 		},
 		text: function (val) {
@@ -79,41 +77,41 @@
 					if(typeof obj === 'string'){
 						return this[i].getAttribute(attr);
 					}else if(typeof obj === 'object'){
-				    for(var attr in obj){
-				      this[i].setAttribute(attr,obj[attr]);
-						}  	
-					}
-				}else{
+    				for(var attr in obj){
+    				  this[i].setAttribute(attr,obj[attr]);
+    				}
+				  }
+				} else {
 		      this[i].setAttribute(attr,val);
 				}
-			}		
+			}
 		},
 		prepend: function(str) {
 			var len = this.length;
   		for (var i = 0; i < len; i++) {
 	      this[i].insertAdjacentHTML('afterbegin', str);
 	    }
-	    return this;
+      return this;
 		},
 		append: function (str) {
-	    var len = this.length;
-  		for (var i = 0; i < len; i++) {
-	      this[i].insertAdjacentHTML('beforeend', str);
-	    }
+      var len = this.length;
+	    for (var i = 0; i < len; i++) {
+         this[i].insertAdjacentHTML('beforeend', str);
+      }
 	    return this;
 		},
 		before: function (str) {
 	    var len = this.length;
-  		for (var i = 0; i < len; i++) {
-	    	this[i].insertAdjacentHTML('beforebegin', str);
-	    }
+	  	for (var i = 0; i < len; i++) {
+		    this[i].insertAdjacentHTML('beforebegin', str);
+		  }
 	    return this;
 		},
 		after: function (str) {
 	    var len = this.length;
-  		for (var i = 0; i < len; i++) {
-	      this[i].insertAdjacentHTML('afterend', str);
-	    }
+      for (var i = 0; i < len; i++) {
+    	  this[i].insertAdjacentHTML('afterend', str);
+    	}
 	    return this;
 		},
 		remove: function () {
@@ -157,26 +155,26 @@
 					}else if(typeof obj === 'object'){
 				    for(var attr in obj){
 							this[i].style[attr] = obj[attr];
-						}  	
+						}
 					}
-				} else {	
+				} else {
 					if(typeof val === 'function'){
 						this[i].style[attr] = val();
 					}else{
 						this[i].style[attr] = val;
-					}   
-		    } 
+					}
+		   	}
 	    }
 			return this;
 		},
-		find: function(selector){			
+		find: function(selector){
 			return this.init(selector,this[0]);
 		},
 		first: function(){
 			return this.init(this[0]);
 		},
 		last: function(){
-			return this.init(this[this.length-1]);
+			return this.init(this[this.length - 1]);
 		},
 		eq: function(index){
 			return this.init(this[index]);
@@ -184,70 +182,61 @@
 		parent: function(){
 			return this.init(this[0].parentNode);
 		}
-	}
-	
+	};
+
 	/**
 	 * DOM ready
 	 * @param {Object} callback
 	 */
-	mjs.ready = function (callback) {
-		var readyRE = /complete|loaded|interactive/;
-		if (readyRE.test(document.readyState)) {
-			callback();
-		} else {
-			document.addEventListener('DOMContentLoaded', function() {
+		mjs.ready = function (callback) {
+			var readyRE = /complete|loaded|interactive/;
+			if (readyRE.test(document.readyState)) {
 				callback();
-			}, false);
-		}
-		return this;
-	};
-	
-	/**
-	 * each 函数
-	 */
-	mjs.each = function (context, item, index){
-	  [].forEach.call(context, function(item, index) {
-      
-    });
-	};
-	
+			} else {
+				document.addEventListener('DOMContentLoaded', function() {
+					callback();
+				}, false);
+			}
+			return this;
+		};
+
   /**
-   * javascript引擎
+ 	 * javascript引擎
    * @param {Object} tpl
    * @param {Object} data
    */
-  mjs.tpl = function(tpl, data) {
-	  var re = /{{(.+?)}}/g,cursor = 0,reExp = /(^( )?(var|if|for|else|switch|case|break|{|}|;))(.*)?/g,	code = 'var r=[];\n';
+		mjs.tpl = function(tpl, data) {
+	  	var re = /{{(.+?)}}/g,cursor = 0,reExp = /(^( )?(var|if|for|else|switch|case|break|{|}|;))(.*)?/g,	code = 'var r=[];\n';
 
-		// 解析html
-		function parsehtml(line) {
-			// 单双引号转义，换行符替换为空格,去掉前后的空格
-			line = line.replace(/('|")/g, '\\$1').replace(/\n/g, ' ').replace(/(^\s+)|(\s+$)/g,"");
-			code +='r.push("' + line + '");\n';
-		}
-		
-		// 解析js代码		
-		function parsejs(line) {   
-			// 去掉前后的空格
-			line = line.replace(/(^\s+)|(\s+$)/g,"");
-		  code += line.match(reExp)? line + '\n' : 'r.push(' + line + ');\n';
-		}	
-	    
-    while((match = re.exec(tpl))!== null) {
-    	// 开始标签  {{ 前的内容和结束标签 }} 后的内容
-    	parsehtml(tpl.slice(cursor, match.index))
-    	// 开始标签  {{ 和 结束标签 }} 之间的内容
-    	parsejs(match[1])
-    	// 每一次匹配完成移动指针
-      cursor = match.index + match[0].length;
-    }
-    // 最后一次匹配完的内容
-    parsehtml(tpl.substr(cursor, tpl.length - cursor));
-    code += 'return r.join("");';
-    return new Function(code.replace(/[\r\t\n]/g, '')).apply(data);
-	}
-  
-	mjs.fn.init.prototype = mjs.fn;	
+			// 解析html
+			function parsehtml(line) {
+				// 单双引号转义，换行符替换为空格,去掉前后的空格
+				line = line.replace(/('|")/g, '\\$1').replace(/\n/g, ' ').replace(/(^\s+)|(\s+$)/g, '');
+				code += 'r.push("' + line + '");\n';
+			}
+
+			// 解析js代码
+			function parsejs(line) {
+				// 去掉前后的空格
+				line = line.replace(/(^\s+)|(\s+$)/g, '');
+			  code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n';
+			}
+
+	    while((match = re.exec(tpl)) !== null) {
+	    	// 开始标签  {{ 前的内容和结束标签 }} 后的内容
+	    	parsehtml(tpl.slice(cursor, match.index));
+	    	// 开始标签  {{ 和 结束标签 }} 之间的内容
+	    	parsejs(match[1]);
+	    	// 每一次匹配完成移动指针
+	      cursor = match.index + match[0].length;
+	    }
+	    // 最后一次匹配完的内容
+	    parsehtml(tpl.substr(cursor, tpl.length - cursor));
+	    code += 'return r.join("");';
+	    return new Function(code.replace(/[\r\t\n]/g, '')).apply(data);
+	};
+
+	mjs.fn.init.prototype = mjs.fn;
 	// 为window全局变量添加mjs对象
 	w.mjs = w.m = mjs;
 })(window);
